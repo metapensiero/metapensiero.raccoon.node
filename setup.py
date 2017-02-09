9e91700acfc5fbc475b288b487775332d9dd098c
Lele Gaifax <lele@metapensiero.it>
2017-02-09 11:27:02 +0100

    Remove import of the codecs.open() function, not needed in Python 3
    Also, remove Python 2 from the classifiers, since this supports only Python 3
    now.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 # :Created:   dom 09 ago 2015 12:57:35 CEST
 # :Author:    Alberto Berti <alberto@arstecnica.it>
 # :License:   GNU General Public License version 3 or later
-# :Copyright: Copyright (C) 2016 Arstecnica s.r.l.
+# :Copyright: Copyright (C) 2016, 2017 Arstecnica s.r.l.
 #
 
 import os
-from codecs import open
 
 from setuptools import setup, find_packages
+
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
@@ -19,13 +19,14 @@ with open(os.path.join(here, 'CHANGES.rst'), encoding='utf-8') as f:
 with open(os.path.join(here, 'version.txt'), encoding='utf-8') as f:
     VERSION = f.read().strip()
 
+
 setup(
     name="raccoon.rocky.node",
     version=VERSION,
     url="https://gitlab.com/arstecnica/raccoon.rocky.node",
 
     description="A base object for publishing WAMP resources",
-    long_description=README + u'\n\n' + CHANGES,
+    long_description=README + '\n\n' + CHANGES,
 
     author="Alberto Berti",
     author_email="alberto@arstecnica.it",
@@ -34,10 +35,9 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         "Programming Language :: Python",
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         ],
     keywords='',
 
